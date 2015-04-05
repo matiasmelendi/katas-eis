c1d9789c7c105943a56c31a385d37b71b928634f
Matias Melendi <mmelendi10pines.com>
2015-04-05 00:05:48 -0300

    refactor

--- char_counter/model/char_counter.rb
+++ char_counter/model/char_counter.rb
@@ -3,10 +3,20 @@ class CharCounter
     def self.count string
       result = Hash.new
 
-      string.delete(' ').each_char do |c|
-        result[c] = string.count c
-      end unless string.nil?
+      count_chars(result, string) unless string.nil?
 
       result
     end
+
+  private
+
+  def self.without_withe_spaces string
+    string.delete(' ')
+  end
+
+  def self.count_chars hash, string
+    without_withe_spaces(string).each_char do |c|
+      hash[c] = string.count c
+    end
+  end
 end
