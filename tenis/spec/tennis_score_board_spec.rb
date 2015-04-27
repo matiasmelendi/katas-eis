e3e7ef3a56bcf3d774f6953250d89e4710681129
memonono94 <mmelendi@10pines.com>
2015-04-27 06:31:03 -0300

    * Spec Refactor

--- tenis/spec/tennis_score_board_spec.rb
+++ tenis/spec/tennis_score_board_spec.rb
@@ -19,89 +19,75 @@ describe TennisScoreBoard do
     expect(score_board.points).to eq [0, 0]
   end
 
-  context "when player wins a game" do
+  describe "when players score" do
 
-    let!(:score_p1) { TennisScoreTest.new ThirtyPoints.new }
-    let!(:score_p2) { TennisScoreTest.new FortyPoints.new  }
+    let(:test_player_1) { TestPlayer.new "test_player_1", score_p1 }
+    let(:test_player_2) { TestPlayer.new "test_player_2", score_p2 }
 
-    let!(:test_player_1) { TestPlayer.new "test_player_1", score_p1 }
-    let!(:test_player_2) { TestPlayer.new "test_player_2", score_p2 }
+    let(:score_board) { TestTennisScoreBoard.new test_player_1, test_player_2 }
 
-    let!(:score_board) { TestTennisScoreBoard.new test_player_1, test_player_2 }
 
-    before :each do
-      score_board.player_two_scored
+    context "when player wins a set" do
+
+      let(:score_p1) { TennisScoreTest.new ThirtyPoints.new, OneGame.new }
+      let(:score_p2) { TennisScoreTest.new FortyPoints.new, FiveGames.new  }
+
+      before :each do
+        score_board.player_two_scored
+      end
+
+      it "should reset games" do
+        expect(score_board.games).to eq [0, 0]
+      end
+
+      it "should reset points" do
+        expect(score_board.points).to eq [0, 0]
+      end
+
     end
 
-    it "should win a game" do
-      expect(score_board.games).to eq [0, 1]
+    context "when player wins a game" do
+
+      let(:score_p1) { TennisScoreTest.new ThirtyPoints.new }
+      let(:score_p2) { TennisScoreTest.new FortyPoints.new  }
+
+      before :each do
+        score_board.player_two_scored
+      end
+
+      it "should win a game" do
+        expect(score_board.games).to eq [0, 1]
+      end
+
+      it "should reset points" do
+        expect(score_board.points).to eq [0, 0]
+      end
     end
 
-    it "should reset points" do
-      expect(score_board.points).to eq [0, 0]
-    end
-  end
+    context "when a player wins the match " do
 
-  context "when player wins a set" do
+      let(:score_p1) { TennisScoreTest.new ThirtyPoints.new, OneGame.new, ZeroSets.new }
+      let(:score_p2) { TennisScoreTest.new FortyPoints.new, FiveGames.new, OneSet.new  }
 
-    let!(:score_p1) { TennisScoreTest.new ThirtyPoints.new, OneGame.new }
-    let!(:score_p2) { TennisScoreTest.new FortyPoints.new, FiveGames.new  }
+      before :each do
+        score_board.player_two_scored
+      end
 
-    let!(:test_player_1) { TestPlayer.new "test_player_1", score_p1 }
-    let!(:test_player_2) { TestPlayer.new "test_player_2", score_p2 }
+      it "should wins the match" do
+        expect(score_board.match_finished?).to be_truthy
+      end
 
-    let!(:score_board) { TestTennisScoreBoard.new test_player_1, test_player_2 }
-
-    before :each do
-      score_board.player_two_scored
     end
 
-    it "should reset games" do
-      expect(score_board.games).to eq [0, 0]
-    end
+    context "when points are 40-40" do
 
-    it "should reset points" do
-      expect(score_board.points).to eq [0, 0]
-    end
+      let(:score_p1) { TennisScoreTest.new FortyPoints.new }
+      let(:score_p2) { TennisScoreTest.new FortyPoints.new  }
 
-  end
+      it "should reset the points scores" do
+        expect(score_board.match_state).to be_instance_of Deuce
+      end
 
-  context "when a player wins the match " do
-
-    let!(:score_p1) { TennisScoreTest.new ThirtyPoints.new, OneGame.new, ZeroSets.new }
-    let!(:score_p2) { TennisScoreTest.new FortyPoints.new, FiveGames.new, OneSet.new  }
-
-    let!(:test_player_1) { TestPlayer.new "test_player_1", score_p1 }
-    let!(:test_player_2) { TestPlayer.new "test_player_2", score_p2 }
-
-    let!(:score_board) { TestTennisScoreBoard.new test_player_1, test_player_2 }
-
-    before :each do
-      score_board.player_two_scored
-    end
-
-    it "should wins the match" do
-      expect(score_board.match_finished?).to be_truthy
-    end
-
-  end
-
-  context "when points are 40-40" do
-
-    let!(:score_p1) { TennisScoreTest.new ThirtyPoints.new }
-    let!(:score_p2) { TennisScoreTest.new FortyPoints.new  }
-
-    let!(:test_player_1) { TestPlayer.new "test_player_1", score_p1 }
-    let!(:test_player_2) { TestPlayer.new "test_player_2", score_p2 }
-
-    let!(:score_board) { TennisScoreBoard.new test_player_1, test_player_2 }
-
-    before :each do
-      score_board.player_one_scored
-    end
-
-    it "should reset the points scores" do
-      expect(score_board.match_state).to be_instance_of Deuce
     end
 
   end
