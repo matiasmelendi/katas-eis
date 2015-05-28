8ccc4e40dff7e869c64887e605119de43b057138
memonono94 <mmelendi@10pines.com>
2015-05-27 23:57:13 -0300

    * Configuración del ambiente modificada, eliminado el required de simplecov.

--- batalla_naval/features/support/env.rb
+++ batalla_naval/features/support/env.rb
@@ -3,22 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + "/../../config/boot")
 require 'capybara/cucumber'
 require 'rspec/expectations'
 
-require 'simplecov'
 require_relative '../../app/batalla_naval'
 
-SimpleCov.start do
-  root(File.join(File.dirname(__FILE__), '..','..'))
-  coverage_dir 'reports/coverage'
-  add_filter '/spec/'
-  add_filter '/features/'
-  add_filter '/admin/'
-  add_filter '/db/'
-  add_filter '/config/'
-  add_group "Models", "app/models"
-  add_group "Controllers", "app/controllers"
-  add_group "Helpers", "app/helpers"
-end
-
-
-#Capybara.default_driver = :selenium
-Capybara.app = BatallaNaval::App.tap { |app|  }
+#Capybara.javascript_driver = :selenium
+#Capybara.app = BatallaNaval::BattleshipApp.tap { |app| }
